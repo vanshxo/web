9b58c61806323a5fee8f1f7d9f7cd7539f50d83d
vanshxo <khatrivansh43@gmail.com>
2024-09-28 22:38:24 +0530

    new

--- index.js
+++ index.js
@@ -27,7 +27,4 @@ app.get('/', (req, res) => {
 
 
 // Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports=app;
