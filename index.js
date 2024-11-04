5035b7d462639564c3b67daa2d8e5c28852acbc1
vanshxo <khatrivansh43@gmail.com>
2024-11-04 19:30:51 +0530

    CV changed

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
-
+// const { connectDB, getDB } = require('./config/db');
 require('dotenv').config()
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// (async()=>{
+//   try{
+//     await connectDB(); // Wait for the connection to complete
+//     const db = getDB(); // Retrieve the database instance
+//     console.log('Database instance retrieved successfully');
+//   }
+//   catch(error){
+//     console.error(`Failed to connect to the database: ${error.message}`);
+//     process.exit(1); // Exit if the connection fails
+//   }
+// })()
+
 
 
 // Serve static files from the 'frontend' directory
@@ -12,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(express.static(path.join(__dirname,'uploads')))
 // Handle the root route to serve the main HTML file
 app.get('/download_resume',(req,res)=>{
-    const file=path.join(__dirname,'uploads','Vansh_Khatri_Resume.pdf');
+    const file=path.join(__dirname,'uploads','Vansh_KhatriCV.pdf');
     res.download(file);
 })
 
@@ -20,11 +34,25 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
+// app.get('/Register',(req,res)=>{
+//   res.sendFile(path.join(__dirname,'frontend','register.html'));
+// })
+// app.get('/Login',(req,res)=>{
+//   res.sendFile(path.join(__dirname,'frontend','login.html'));
+// })
 
-
-
+// app.post('/Login',(req,res)=>{
+//   res.send({'unavailable':'users'});
+// })
+// app.post('/Register',(req,res)=>{
+//   const fields=req.body;
+//    res.send({'unavailable':'users'});
+   
+  
+// })
 
 
 
 // Start the server
+app.listen(8000,()=>{console.log('server has started on localhost:8000')})
 module.exports=app;
